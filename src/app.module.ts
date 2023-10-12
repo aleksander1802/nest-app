0809b969227fe439403bb990e9a4eebcf93df871
Alex2 <aleksander1802@gmail.com>
2023-10-12 15:23:39 +0300

    feat: implemented config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { AuthModule } from './auth/auth.module';
 import { NestPageModule } from './nest-page/nest-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [AuthModule, NestPageModule, ProductModule, ReviewModule],
+    imports: [
+        ConfigModule.forRoot(),
+        AuthModule,
+        NestPageModule,
+        ProductModule,
+        ReviewModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
