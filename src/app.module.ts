b1da68eef432a14f5d4150104d48b37e48b55143
Alex2 <aleksander1802@gmail.com>
2023-10-12 16:47:48 +0300

    feat: implemented mongoose, updated app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,18 @@ import { AuthModule } from './auth/auth.module';
 import { NestPageModule } from './nest-page/nest-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypegooseModule } from '@m8a/nestjs-typegoose';
+import { getMongoConfig } from './configs/mongo.config';
 
 @Module({
     imports: [
         ConfigModule.forRoot(),
+        TypegooseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: getMongoConfig,
+        }),
         AuthModule,
         NestPageModule,
         ProductModule,
