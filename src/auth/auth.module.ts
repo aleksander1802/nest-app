3bb194a7d8a3a90657300cd7d845eb188c07b4ae
Alex2 <aleksander1802@gmail.com>
2023-10-13 19:10:18 +0300

    feat: added jwt authorization

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,9 @@ import { AuthController } from './auth.controller';
 import { TypegooseModule } from '@m8a/nestjs-typegoose';
 import { UserModel } from './user.model';
 import { AuthService } from './auth.service';
+import { getJWTConfig } from 'src/configs/jwt.config';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
     controllers: [AuthController],
@@ -15,6 +18,11 @@ import { AuthService } from './auth.service';
                 },
             },
         ]),
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: getJWTConfig,
+        }),
     ],
     providers: [AuthService],
 })
