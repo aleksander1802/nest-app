9b8ec5760aaec06dd8fb150ad76a46e10c63a609
Alex2 <aleksander1802@gmail.com>
2023-10-15 15:15:26 +0300

    feat: added files module, controller, service

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { ReviewModule } from './review/review.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypegooseModule } from '@m8a/nestjs-typegoose';
 import { getMongoConfig } from './configs/mongo.config';
+import { FilesModule } from './files/files.module';
 
 @Module({
     imports: [
@@ -21,6 +22,7 @@ import { getMongoConfig } from './configs/mongo.config';
         NestPageModule,
         ProductModule,
         ReviewModule,
+        FilesModule,
     ],
     controllers: [AppController],
     providers: [AppService],
